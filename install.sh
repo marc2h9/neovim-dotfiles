370e22ef33e6d949298efd4b2dda08e3d7f20265
marc2h9 <marcverstegen09@gmail.com>
2025-08-25 22:08:56 +0200

    Added a install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+fi
+
+if [ "$OS" = "Ubuntu" ]; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+  sudo apt-get update
+  sudo apt-get install curl tar ripgrep -y
+  snap install node
+
+  USER=$(whoami)
+  ln -s /home/neovim-dotfiles/nvim/ /home/$USER/.config/
+  clear
+  echo CONFIG SUCCESSFULLY INSTALLED!
+  echo DO NOT DELETE THE CURRENT DIRECTORY
+
+elif [ "$OS" = "Arch" ]; then
+  echo "Arch support will be added later"
+fi
