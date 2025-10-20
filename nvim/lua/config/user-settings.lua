098d1dbf1892311be2f91fdfbde01a23bb6958d9
marc2h9 <marcverstegen09@gmail.com>
2025-10-20 12:06:15 +0200

    Added Telescope.nvim Lazy.nvim Dashboard.nvim lualine.nvim

---
+++ nvim/lua/config/user-settings.lua
@@ -0,0 +1,10 @@
+-- Setting colorscheme
+vim.cmd('colorscheme onedark')
+
+-- Setting tabs
+vim.cmd('set shiftwidth=2')
+vim.cmd('set tabstop=2')
+
+-- Setting linenumbers
+vim.cmd('set nu')
+vim.cmd('set rnu')
